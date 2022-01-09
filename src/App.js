b17dadde9235ba1e97138d91d1d3fe9a23f059d7
Mahmoud Triki <Mahmoud.Triki@vistaprint.com>
2022-01-09 15:05:00 +0100

    second

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload now!.
+          Edit <code>src/App.js</code> and save to reload now from second!.
         </p>
         <a
           className="App-link"
